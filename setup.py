d27ccb324b0fc2b7c7d05bec183155b3439dde7b
randolph <49714224+goophps@users.noreply.github.com>
2022-08-18 03:30:54 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rongo",
+    version="0.0.1",
+    author="官迪",
+    author_email="goophps@gmail.com",
+    description="This is not a python web framework, it's a complement to such mini frameworks like fastapi sanic. Mainly refer to Thinkphp Laravel Django Masonite.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/goophps/rongo.git",
+    # 列出这个项目的包
+    packages=[
+      "rongo",
+      "rongo.cache.drivers",
+      "rongo.cache",
+      "rongo.utils",
+    ],
+    # 依赖的包，将自动安装
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
